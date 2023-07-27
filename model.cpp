fc8252374f34fefb59a0cd0bb62749eb848e8001
DaneLin <825658797@qq.com>
2023-07-27 10:01:57 +0800

    成功实现lesson3的课后作业

--- model.cpp
+++ model.cpp
@@ -9,10 +9,14 @@
  * 20230725 author: lin
  * 1、更新了obj文件中f的读取，能够写入对应f的vert，tex_vert, norm_vert;
 */
-Model::Model(const char *filename) : verts_(), faces_(), norms_(), uv_() {
+Model::Model(const char *filename) : verts_(), faces_() {
     std::ifstream in;
     in.open (filename, std::ifstream::in);
-    if (in.fail()) return;
+    if (in.fail()) 
+    {
+        std::cerr << "Unable to open file!" << std::endl;
+        return;
+    }
     std::string line;
     while (!in.eof()) {
         std::getline(in, line);
@@ -23,77 +27,91 @@ Model::Model(const char *filename) : verts_(), faces_(), norms_(), uv_() {
             Vec3f v;
             for (int i=0;i<3;i++) iss >> v.raw[i];
             verts_.push_back(v);
-        } 
-        else if(!line.compare(0, 3, "vt ")) {
-            iss >> trash >> trash;
-            Vec2f uv;
-            for (int i = 0; i < 2; i++) iss >> uv[i];
-            uv_.push_back(uv);
-        }
-        else if (!line.compare(0, 3, "vn ")) {
-            iss >> trash >> trash;
-            Vec3f normal;
-            for (int i = 0; i < 3; i++) iss >> normal[i];
-            norms_.push_back(normal);
-        }
-        else if (!line.compare(0, 2, "f ")) {
-            std::vector<Vec3i> f;
-            Vec3i tmp;
+        } else if (!line.compare(0, 2, "f ")) {
+            std::vector<int> f;
+            std::vector<int> vt;
+            std::vector<int> vn;
+            int idx_f, idx_vt, idx_vn;
             iss >> trash;
-            while (iss >> tmp[0] >> trash >> tmp[1] >> trash >> tmp[2]) {
-                for (int i = 0; i < 3; i++) tmp[i]--;
-                f.push_back(tmp);
+            while (iss >> idx_f >> trash >> idx_vt >> trash >> idx_vn) {
+                idx_f--; // in wavefront obj all indices start at 1, not zero
+                idx_vt--;
+                idx_vn--;
+                f.push_back(idx_f);
+                vt.push_back(idx_vt);
+                vn.push_back(idx_vn);
+                //std::cout << idx_f << ' ' << idx_vt << ' ' << idx_vn << std::endl;
             }
             faces_.push_back(f);
+            fuvs_.push_back(vt);
+            fnorms_.push_back(vn);
+        } else if (!line.compare(0, 3, "vt ")) {
+            //std::cout << line << std::endl;
+            iss >> trash >> trash;
+            //std::cout << trash << std::endl;
+            Vec2f vt;
+            float ftrash;
+            for (int i = 0; i < 3; i++) 
+            {
+                if (i == 2)
+                {
+                    iss >> ftrash;
+                    //std::cout << ftrash << std::endl;
+                    continue;
+                } 
+                iss >> vt.raw[i];
+                //std::cout << vt.raw[i] << ' ';
+            }
+            //std::cout << std::endl;
+            uvs_.push_back(vt);
+        } else if (!line.compare(0, 3, "vn ")) {
+            iss >> trash >> trash;
+            //std::cout << trash << std::endl;
+            Vec3f vn;
+            for (int i = 0; i < 3; i++) 
+            {
+                iss >> vn.raw[i];
+                //std::cout << vn.raw[i] << ' ';
+            }
+            //std::cout << std::endl;
+            norms_.push_back(vn);
         }
     }
-    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << " vt# " << uv_.size() << " vn# " << norms_.size() << std::endl;
-    loadTexture(filename, "_diffuse.tga", diffuseMap_);
+    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
 }
-
 
 Model::~Model() {
 }
-
+//返回模型顶点个数
 int Model::nverts() {
     return (int)verts_.size();
 }
-
+//返回模型表面个数
 int Model::nfaces() {
     return (int)faces_.size();
 }
-
+//获取idx的表面
 std::vector<int> Model::face(int idx) {
-    std::vector<int> face;
-    std::vector<Vec3i> tmp = faces_[idx];
-    for (int i = 0; i < tmp.size(); i++)
-        face.push_back(tmp[i][0]);
-    return face;
+    return faces_[idx];
 }
 
 Vec3f Model::vert(int i) {
     return verts_[i];
 }
 
-void Model::loadTexture(std::string filename, const char* suffix, TGAImage& image)
-{
-    std::string texfile(filename);
-    size_t dot = texfile.find_last_of(".");
-    if (dot != std::string::npos) {
-        texfile = texfile.substr(0, dot) + std::string(suffix);
-        std::cerr << "texture file " << texfile << " loading " << (image.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
-        image.flip_vertically();
-    }
+Vec2i Model::uv(int i) {
+    return Vec2i(uvs_[i].x * 1024,uvs_[i].y * 1024);
 }
 
-TGAColor Model::diffuse(Vec2i uv)
-{
-    return diffuseMap_.get(uv.x, uv.y);
+Vec3f Model::norm(int i) {
+    return norms_[i];
 }
 
-Vec2i Model::uv(int iface, int nvert)
-{
-    int idx = faces_[iface][nvert][1];
-    return Vec2i(uv_[idx].x * diffuseMap_.get_width(), uv_[idx].y * diffuseMap_.get_height());
+std::vector<int> Model::fuvs(int idx) {
+    return fuvs_[idx];
+}
+
+std::vector<int> Model::fnorms(int idx) {
+    return fnorms_[idx];
 }
 
--- model.h
+++ model.h
@@ -3,36 +3,27 @@
 
 #include <vector>
 #include "geometry.h"
-#include "tgaimage.h"
 
 class Model {
 private:
-	// std::vector<Vec3f> verts_;
-	// std::vector<Vec2f> uvs_;
-	// std::vector<Vec3f> norms_;
-	// std::vector<std::vector<int>> faces_;//每个面是如何组成的，对应的v，vt，vn
-	// std::vector<std::vector<int>> fuvs_;
-	// std::vector<std::vector<int>> fnorms_;
-	// TGAImage diffuseMap_;
 	std::vector<Vec3f> verts_;
-	std::vector<std::vector<Vec3i>> faces_;
+	std::vector<Vec2f> uvs_;
 	std::vector<Vec3f> norms_;
-	std::vector<Vec2f> uv_;
-	TGAImage diffuseMap_;
-	void loadTexture(std::string filename, const char* suffix, TGAImage& image); 
+	std::vector<std::vector<int>> faces_;//每个面是如何组成的，对应的v，vt，vn
+	std::vector<std::vector<int>> fuvs_;
+	std::vector<std::vector<int>> fnorms_; 
 public:
 	Model(const char *filename);
 	~Model();
 	int nverts();//获取顶点个数
 	int nfaces();//获取面的个数
 	Vec3f vert(int i);
-	Vec2f uv(int i);
+	Vec2i uv(int i);
 	Vec3f norm(int i);
 	std::vector<int> fuvs(int idx);
 	std::vector<int> fnorms(int idx);
 	std::vector<int> face(int idx);
-	TGAColor diffuse(Vec2i uv);
-	Vec2i uv(int iface, int nvert);
+	
 };
 
 #endif //__MODEL_H__
